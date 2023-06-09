a8f90aba7541f2e0983c0fbb723e457f9bc59b45
jgonz671 <jgonz671@ucr.edu>
2023-06-09 01:22:52 -0700

    item update

--- libFiles/character.h
+++ libFiles/character.h
@@ -22,18 +22,9 @@ class Character : public Entity {
         int getCurrency();
         void addCurrency(int amount); 
         void addItemToInventory(Item*); 
+        void useItemFromInventory(int );
         void listInventory();
-<<<<<<< HEAD
-<<<<<<< HEAD
-        void useItemFromInventory(int index);
-        //virtual void reset_values() = 0; 
-=======
         virtual void resetValues() = 0;
->>>>>>> 41c710bf1a57ea0c2688a1d2a6af7c8b4725b1cc
-=======
-        void useItemFromInventory(int index);
-        virtual void resetValues() = 0;
->>>>>>> a56e62d69e3e788bc5c3ae708fb8ed1e1071a180
 };
 
 #endif
